f445cf83628d64e203531ca58d548c990a3031d6
Richard Galvez <richardalbertogalvez@gmail.com>
2025-12-28 00:07:00 -0500

    Started basic logic for showing the list of current videos and adding to the list (non-persistent).

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # YouQueue
-Connect and add videos to a shared playlist to be played in desired order on another device.
+
+Connect and add videos to a shared YouTube playlist to be played in desired order on another device.
+
+1. QR Code scan to go to app.
+
+2. Taken to 'Playlist View' (Main Application view).
+
+3. Click 'Add to Queue' button - Option to search for video to play comes up.
+
+4. After desired video shown, 'Add video' button available.
+
+5. Video is added to list.
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+video_list = []
+
+class Video(BaseModel):
+    id: int
+    name: str
+    link: str
+
 @app.get("/")
 def home():
-    return {"Hello" : "World!"}
+    message = "Welcome to YouQueue!"
+    return {"Message": message}
+
+# TODO: Show list of current videos
+@app.get("/videos")
+def get_video_list():
+    return video_list
+
+# TODO: Add videos to list of current videos
+@app.post("/videos")
+def add_videos(video: Video):
+    video_list.insert(video.id, video.name)
+    return {"Current List of Videos": video_list}
